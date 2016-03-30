5a6cac861fb84c6654eb6dae20cf4d9445374d13
Jason Yao <jasony.edu@gmail.com>
2016-03-30 02:32:04 -0400

    Added automatic symlink creation script

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -e
+
+##
+# Setup script to create the necessary symlinks to make things in the future easier (can also be manually done)
+##
+
+###
+# Helper functions
+##
+info () {
+    printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+user () {
+    printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+success () {
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+absRootDir="$(pwd)"
+
+# Checks if recitation symlink is already installed
+recitations_link="$absRootDir/recitations/JavaBoilerPlate"
+    if [ -d "$recitations_link" ];
+    then
+        info "Recitations symlink already installed"
+    else
+        ln -s $absRootDir/JavaBoilerplate/ $recitations_link
+        success "Recitations symlink installed"
+    fi
+
+# Checks if homework symlink is already installed
+homeworks_link="$absRootDir/homeworks/JavaBoilerPlate"
+    if [ -d "$homeworks_link" ];
+    then
+        info "Homework symlink already installed"
+    else
+        ln -s $absRootDir/JavaBoilerplate/ $homeworks_link
+        success "Homeworks symlink installed"
+    fi
