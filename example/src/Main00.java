d8567c25493e52f1a69610c62aa86d21762a4a00
Jason Yao <jasony.edu@gmail.com>
2016-03-30 01:59:39 -0400

    Added boilerplate files

---
+++ example/src/Main00.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main01
+{
+    public static void main(String args[])
+    {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder out = new StringBuilder();
+        try
+        {
+
+			System.out.print(out);
+        }
+        catch (IOException e)
+        {e.printStackTrace();}
+
+    } // End of the main method
+} // End of the main class
