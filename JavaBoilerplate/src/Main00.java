76708c819953a1e476fb1a83d31213409250183c
Jason Yao <jasony.edu@gmail.com>
2016-03-30 02:10:17 -0400

    Added testing suite and output folder structure

--- JavaBoilerplate/src/Main00.java
+++ JavaBoilerplate/src/Main00.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main01
+public class Main00
 {
     public static void main(String args[])
     {
