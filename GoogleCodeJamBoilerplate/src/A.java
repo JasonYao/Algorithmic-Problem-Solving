01d347d0dfe3de6d9ae4edba649acab9e7739f33
Jason Yao <jasony.edu@gmail.com>
2016-03-30 02:08:06 -0400

    Added google code jam style boilerplate

---
+++ GoogleCodeJamBoilerplate/src/A.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A
+{
+    public static void main(String args[])
+    {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder out = new StringBuilder();
+
+        try
+        {
+            int testCases = Integer.parseInt(br.readLine());
+
+            for (int testCase = 0; testCase < testCases; ++testCase)
+            {
+
+            } // End of iterating through all test cases
+
+			System.out.print(out);
+        }
+        catch (IOException e)
+        {e.printStackTrace();}
+    } // End of the main method
+} // End of the main class
